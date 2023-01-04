01ac1c215fafcc2aaa21ca3005a74c9623e38859
Om Shanmukha <52312761+Shanikata@users.noreply.github.com>
2023-01-04 17:50:26 +0530

    Add files via upload
    The getGroupedCoordinates method for the LineSegment class is designed to return a list of collections of Coordinate objects representing points along the line segment.

---
+++ LineSegment Assignment_V3_Final.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+from typing import List
+import math
+
+
+# Coordinate class: Takes 2 float values for x & y
+class Coordinate:
+    def __init__(self, x : float, y : float) -> None:
+        self.X = x
+        self.Y = y
+        
+    def DistanceTo(self, otherCoordinate : Coordinate):
+        deltaX = otherCoordinate.X - self.X
+        deltaY = otherCoordinate.Y - self.Y
+        return math.sqrt(deltaX * deltaX + deltaY * deltaY)
+    
+    
+#  LineSegment class; Takes 2 coordinates as start point & end point
+class LineSegment:
+    def __init__(self, startpoint : Coordinate, endpoint : Coordinate) -> None:
+        self.StartPoint = startpoint
+        self.EndPoint = endpoint
+        self.Length = startpoint.DistanceTo(endpoint)
+
+    def getGroupedCoordinates(self, groupSize: int, spacing: float, 
+                              gap: float) -> List[List[Coordinate]]:
+        """Returns a list of collections of coordinates along the line segment.
+    
+        Args:
+            groupSize (int): The number of points to place in each collection.
+            spacing (float): The spacing between points in the same collection.
+            gap (float): The gap between collections.
+        
+        Returns:
+            List[List[Coordinate]]: A list of lists of Coordinate objects representing the points along the line segment.
+        """
+        # Calculate the difference in x and y between the start and end points
+        dx = self.EndPoint.X - self.StartPoint.X
+        dy = self.EndPoint.Y - self.StartPoint.Y
+
+        # Calculate the increment in x and y for each point along the line segment
+        x_inc = (dx + gap) / (groupSize - 1)
+        y_inc = (dy + gap) / (groupSize - 1)
+
+        # Initialize a list to store the coordinates
+        coordinates = []
+
+        # Calculate the total number of groups that can be placed along the line segment
+        num_groups = int((self.Length - spacing) / (spacing + gap))
+
+        # Iterate through the number of groups and calculate the coordinates of each point
+        for i in range(num_groups):
+            group = []
+            for j in range(groupSize):
+                x = self.StartPoint.X + i * (spacing + gap) + j * x_inc
+                y = self.StartPoint.Y + i * (spacing + gap) + j * y_inc
+                group.append(Coordinate(x, y))
+            coordinates.append(group)
+    
+        # Return the list of coordinates
+        return coordinates
+
+
+# Create a start point and an end point
+startpoint = Coordinate(0, 0)
+endpoint = Coordinate(10, 10)
+
+# Create a line segment object
+segment = LineSegment(startpoint, endpoint)
+
+# Get the grouped coordinates along the line segment
+coordinates = segment.getGroupedCoordinates(5, 2, 1)
+
+# Print the grouped coordinates to the console
+print(coordinates)
